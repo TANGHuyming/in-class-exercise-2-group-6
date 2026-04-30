1deaaa22da91c8d5ed189a7ef976bad7f8d6e3d3
FirstPotatoCoder <sopanhaream@gmail.com>
2026-04-30 14:17:20 +0700

    Login Register UI

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // GET home page
-router.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Express + Handlebars',
-    message: 'Welcome to your Express Handlebars app!'
-  });
+// router.get("/", (req, res) => {
+//   res.render("index", {
+//     title: "Express + Handlebars",
+//     message: "Welcome to your Express Handlebars app!",
+//   });
+// });
+
+// Login page
+router.get("/", (req, res) => {
+  res.render("auth/login", { title: "Login" });
 });
+
+router.get("/login", (req, res) => {
+  res.render("auth/login", { title: "Login" });
+});
+
+// Register page
+router.get("/register", (req, res) => {
+  res.render("auth/register", { title: "Register" });
+});
+
+// Dashboard
 
 module.exports = router;
