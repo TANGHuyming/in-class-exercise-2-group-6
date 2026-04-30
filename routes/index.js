4d5a0f190fc1b94f0ee38bbaf1fe901dad522911
Reaksmey <64191668+DuckFromAPond@users.noreply.github.com>
2026-04-30 14:17:25 +0700

    route done

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,20 @@ const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
 const publicController = require("../controllers/public.controller");
 
+
+router.get("/register ", publicController.showLogin);
+router.get("/login", publicController.showRegister);          // return JWT
+
 // Home 
 router.get("/", protect, publicController.home);
 router.get("/home", protect, publicController.home);
 
-router.get("/", protect, publicController.home);
-router.get("/home", protect, publicController.home);
+// CRUD records 
+router.get("/api/records", protect, publicController.getRecords);
+router.post("/api/records", protect, publicController.postRecords);
+router.put("/api/records", protect, publicController.putRecords);
+router.delete("/api/records", protect, publicController.deleteRecords);
 
+router.use(publicController.notFound);
 
 module.exports = router;
